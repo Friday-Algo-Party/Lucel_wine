086a87a5ffd5cf5e0da54f20513c085c756ff803
LouiIII3 <jikdsunghuilee@naver.com>
2024-08-07 06:55:29 +0900

    p1546 BufferedReader를 쓴 경우

--- src/p1546/Main.java
+++ src/p1546/Main.java
@@ -1,7 +1,29 @@
 package p1546;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String[] preGradeArray = br.readLine().split(" ");
+        int[] gradeArray = new int[n]; //성적이 들어간 배열
+
+        for(int i = 0; i < preGradeArray.length; i++){
+            gradeArray[i] = Integer.parseInt(preGradeArray[i]);
+        }
+
+        Arrays.sort(gradeArray);
+
+        float maxGrade = gradeArray[n - 1];
+        float sumGrade = 0;
+
+        for(int j = 0; j < gradeArray.length; j++){
+            sumGrade += gradeArray[j] / maxGrade * 100;
+        }
+        System.out.println(sumGrade / n);
     }
 }
