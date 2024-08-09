d3fd78bf4cad0b1e7c7ca6f3ea7eea6f031e7d92
LouiIII3 <jikdsunghuilee@naver.com>
2024-08-09 17:40:56 +0900

    p2839 추가

---
+++ src/p1463/Maintest.java
@@ -0,0 +1,13 @@
+package p1463;
+import java.util.*;
+import java.io.*;
+
+public class Maintest {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+         int a = Integer.parseInt(br.readLine());
+         int dp[] = new int[a + 1];
+
+
+    }
+}
---
+++ src/p2839/Maintest1.java
@@ -0,0 +1,31 @@
+package p2839;
+
+import java.io.*;
+import java.util.*;
+
+public class Maintest1 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int num = Integer.parseInt(st.nextToken());
+
+        int count = 0;
+
+        while (num > 0) {
+            if (num < 3) {
+                System.out.println("-1");
+                return;
+            }
+
+            if (num % 5 == 0) {
+                count += num / 5;
+                System.out.println(count);
+                return;
+            }
+            num-=3;
+            count++;
+        }
+        System.out.println(count);
+    }
+}
