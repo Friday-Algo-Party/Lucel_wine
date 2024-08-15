016d86d0a715e3ab8a5ac99b32a860eeaa6cde67
LouiIII3 <jikdsunghuilee@naver.com>
2024-08-15 15:36:05 +0900

    p1560번 푸는중

---
+++ src/p15650/Maintest1.java
@@ -0,0 +1,24 @@
+package p15650;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class Maintest1 {
+
+    public static int[] arr;
+    public static int N, M;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        //dfs 구현 예정
+    }
+}
